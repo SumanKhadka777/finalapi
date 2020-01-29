0a22552c9c1e0f9918446d91c192979b2bfb2145
SumanKhadka777 <Khadkasuman777@gmail.com>
2020-01-29 08:26:12 +0545

    server starting

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
+
+
+
+
+const app = express();
+
+
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then((db) => {
+        console.log("Successfully connected to MongodB server");
+    }, (err) => console.log(err));
+
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is running at localhost:${process.env.PORT}`);
+    });
